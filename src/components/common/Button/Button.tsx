ca02ac97296cab8cd413b19f3d3f7d3371858776
pjainxido <pjainxido@gmail.com>
2022-07-14 20:32:41 +0900

    feat: Add Button Components

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import styled from '@emotion/styled';
 
 interface ButtonStyle {
   width?: string;
@@ -11,8 +12,31 @@ interface ButtonStyle {
   fontSize?: string;
 }
 
-const Button = () => {
-  return <div>Button</div>;
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement>, ButtonStyle {
+  children: ReactNode;
+  className?: string;
+}
+
+const Button: React.FC<Props> = ({ className, children, ...restProps }) => {
+  return (
+    <StyledButton className={className} {...restProps}>
+      {children}
+    </StyledButton>
+  );
 };
+
+const StyledButton = styled.button`
+  background-color: #0074e9;
+  color: #fff;
+  box-shadow: inset 0 -2px 0 rgb(0 0 0 / 38%);
+  padding: 16px 17px;
+  font-family: apple sd gothic neo, malgun gothic, nanumbarungothic, nanumgothic, dotum, sans-serif;
+  font-size: 17px;
+  line-height: 20px;
+  display: block;
+  padding-left: 0;
+  padding-right: 0;
+  width: 100%;
+`;
 
 export default Button;
